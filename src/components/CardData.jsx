14e10bf7edda749233a84fefce03a755ea3cdcd6
euconectei <rafaelfms@gmail.com>
2020-02-29 01:26:52 -0300

    estilizando página; estilizando card do planeta.

---
+++ src/components/CardData.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import fontawesome from '@fortawesome/fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCloudSunRain,
+  faArrowsAltH,
+  faFilm,
+  faTree,
+  faWater,
+  faSmile,
+  faSync,
+  faUndo,
+  faMale,
+} from '@fortawesome/free-solid-svg-icons';
+
+fontawesome.library.add(
+  faCloudSunRain,
+  faArrowsAltH,
+  faFilm,
+  faTree,
+  faWater,
+  faSmile,
+  faSync,
+  faUndo,
+  faMale,
+);
+
+const GroupStyled = styled.div`
+  border: 2px solid #E7250A;
+  padding: 10px;
+  margin: 5px;
+
+  &:hover {
+    background-color: #B70000;
+    color: #FFF;
+  }
+`;
+
+const IconStyled = styled.span`
+  display: inline-block;
+  height: 20px;
+  margin-right: 10px;
+  text-align: center;
+  width: 20px;
+`;
+
+const CardData = ({ icon, title, value }) => (
+  <GroupStyled>
+    <IconStyled>
+      <FontAwesomeIcon icon={icon} title={title} />
+    </IconStyled>
+    {value}
+  </GroupStyled>
+);
+
+export default CardData;
--- src/pages/Planet.jsx
+++ src/pages/Planet.jsx
@@ -4,15 +4,24 @@ import styled from 'styled-components';
 import PlanetService from '../services/PlanetService';
 import { Loading } from '../components';
 import numbers from '../utils/numbers';
+import CardData from '../components/CardData';
 
 const PlanetStyled = styled.div`
   align-self: center;
-  border: 1px solid red;
+  border: 2px solid #E7250A;
+  color: #B70000;
+  font-family: 'Nova Flat', Helvetica, Arial, sans-serif;
   justify-self: center;
-`;
+  width: 400px;
+  `;
 
-const TitleStyled = styled.div``;
-const DataStyled = styled.div``;
+const TitleStyled = styled.div`
+  color: #B70000;
+  font-size: 24px;
+  padding: 10px;
+  margin: 5px;
+  text-align: center;
+`;
 
 const Planet = () => {
   const [planet, setPlanet] = useState({});
@@ -33,17 +42,17 @@ const Planet = () => {
   return (
     <>
       <PlanetStyled>
-        {
-          Object.keys(planet).map(function (key, i) {
-            if (key === 'name') return (<TitleStyled key={i}>{(planet.name).toUpperCase()}</TitleStyled>)
-            return (<DataStyled key={i}>{key.toUpperCase()}: {planet[key]}</DataStyled>)
-
-            // TODO: Criar lista de filmes
-            // TODO: Criar lista de habitantes
-            // TODO: Tratar dia/hora
-
-          })
-        }
+        <TitleStyled>{(planet.name).toUpperCase()}</TitleStyled>
+        <CardData icon="undo" title="Rotation Period" value={(parseInt(planet.rotation_period)).toLocaleString()} />
+        <CardData icon="sync" title="Orbital Period" value={(parseInt(planet.orbital_period)).toLocaleString()} />
+        <CardData icon="arrows-alt-h" title="Diameter" value={(parseInt(planet.diameter)).toLocaleString()} />
+        <CardData icon="cloud-sun-rain" title="Climate" value={planet.climate} />
+        <CardData icon="smile" title="Gravity" value={planet.gravity} />
+        <CardData icon="tree" title="Terrain" value={planet.terrain} />
+        <CardData icon="water" title="Surface Water" value={planet.surface_water} />
+        <CardData icon="smile" title="Population" value={(parseInt(planet.population)).toLocaleString()} />
+        <CardData icon="male" title="Residents" value={planet.residents} />
+        <CardData icon="film" title="Films" value={planet.films} />
       </PlanetStyled>
       <Link to={`/planets/${numbers.random(61, 1)}`}>Next</Link>
     </>
