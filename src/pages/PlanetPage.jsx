54d364abae6978b73a23c8cdb765ea5fc8b43e43
euconectei <rafaelfms@gmail.com>
2020-03-02 22:32:53 -0300

    implementado botao de volta para home

--- src/pages/PlanetPage.jsx
+++ src/pages/PlanetPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import PlanetService from '../services/PlanetService';
 import { Loading, Button, Card, CardFlip } from '../components';
 
@@ -40,9 +41,33 @@ const PlanetPage = () => {
     setLoading(true);
     history.push(`/planets/${numbers.random(61, 1)}`);
   };
+  const backHome = () => {
+    setLoading(true);
+    history.push(`/`);
+  };
+  const BackBtn = styled.button`
+    position: absolute;
+    top: 10px;
+    left: 10px;
+
+    background-color: transparent;
+    border: 2px solid #666;
+    border-radius: 5px;
+    display: inline-block;
+    margin: 10px 0;
+    padding: 5px;
+    text-align: center;
+    align-self: center;
+
+    color: #666;
+    font-family: 'Nova Flat', Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    outline: none;
+  `;
 
   return (
     <>
+      <BackBtn onClick={() => backHome()}>◀ Home</BackBtn>
       <CardFlip
         flipped={loading}
         back={
